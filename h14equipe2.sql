8ae3590fa59086866f3942d9b34f049090bdc090
Emilie <emilie317@hotmail.com>
2014-02-07 11:51:24 -0500

    visuel page présentation

--- h14equipe2.sql
+++ h14equipe2.sql
@@ -52,7 +52,93 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `image` blob NOT NULL,
   `etat` text NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;page verif
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost:3306
+-- Généré le : Ven 07 Février 2014 à 10:26
+-- Version du serveur: 5.5.35
+-- Version de PHP: 5.3.10-1ubuntu3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `h14equipe2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres`
+--
+
+CREATE TABLE IF NOT EXISTS `offres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fkutilisateurs` int(11) NOT NULL,
+  `fkproduit` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `montant` float NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(25) NOT NULL,
+  `prixdedepart` float NOT NULL,
+  `date` date NOT NULL,
+  `duree` int(11) NOT NULL,
+  `vendu` tinyint(1) NOT NULL,
+  `description` text NOT NULL,
+  `image` blob NOT NULL,
+  `etat` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;page verif
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `adresse` varchar(200) NOT NULL,
+  `codepostal` char(7) NOT NULL,
+  `telephone` varchar(16) NOT NULL,
+  `portemonaie` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `pseudo`, `password`, `email`, `adresse`, `codepostal`, `telephone`, `portemonaie`) VALUES
+(1, 'root', 'admin', 'benjamin.bercy@orange.fr', '9,Impasse des peupliers', '12450', '0565694979', 128.9);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 -- --------------------------------------------------------
 
