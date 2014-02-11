17ba2d75b087b3ce293e2ed0f06bb021047eac5e
benjaminbercy <benjamin.bercy@orange.fr>
2014-02-11 11:49:41 -0500

    ajout de la page d'ajout de produit et vérification + enregistrement

--- verificationproduit.php
+++ verificationproduit.php
@@ -14,6 +14,15 @@
  */
 include "./include/mysqlapi.inc.php";
 insproduit($_POST['nom'],$_POST['prix'],$_POST['duree'],$_POST['description'],$_POST['etat']);
+if(isset($_FILES['avatar']))
+{
+    $extensions = array('.png', '.gif', '.jpg', '.jpeg');
+    $dossier = 'img/produit/';
+    $fichier = $_POST['nom'].'.jpg';
+    if (in_array(strrchr($_FILES['image']['name'], '.'), $extensions)) {
+    move_uploaded_file($_FILES['image']['tmp_name'], $dossier . $fichier);
+    }
+}
 ?>
 <h2>Le Produit à été mis en vente</h2>
 <a href=\'index.php\'><button>Retour</button></a>
