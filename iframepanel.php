7fad5611b0c07edd7b2853192b2a228232ec189a
benjaminbercy <benjamin.bercy@orange.fr>
2014-02-21 09:30:46 -0500

    système de payment + champ recherche

--- iframepanel.php
+++ iframepanel.php
@@ -1,5 +1,5 @@
 <head>
-    <base target="_parent" />
+    <base target="_parent"/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/Projet.css">
@@ -9,28 +9,34 @@
 </head>
 </head>
 <body>
-    <div class="navbar">
-        <div class="navbar-inner">
+<div class="navbar">
+    <div class="navbar-inner">
 
-<?php
-session_start();
-$Titre = $_GET['titre'];
-$Session = $_SESSION['pseudo'];
+        <?php
+        session_start();
+        $Titre = $_GET['titre'];
+        $Session = $_SESSION['pseudo'];
 
-echo "<a class='brand' href='#'>$Titre</a>";
-echo"<ul class='nav'>";
-if (isset($_SESSION['userid'])){
-        echo "<a class='brand' href='#'>$Session</a>";
-        echo "<li><h2>".$_SESSION['argent']."$</h2></li>";
-        echo "<li><a href=ajouterauportemonaie.php><button class='btn btn-warning text-center'>Aprovisionner</button></a></li>";
-        echo "<li><a href=deconnexion.php><button class='btn btn-danger text-center'>Se déconnecter</button></a></li>";
-} else {
-    echo "<li><a href=authentification.html><button class='btn btn-success '>Se connecter</button></a></li>";
-    echo "<li><a href=inscription.html><button class='btn btn-info text-center'>Inscription</button></a></li>";
-}
-?>
+        echo "<a class='brand' href='#'>$Titre</a>";
+        echo "<ul class='nav'>";
+        if (isset($_SESSION['userid'])) {
+            echo "<a class='brand' href='#'>$Session</a>";
+            echo "<li><a class='brand' href='#'>" . $_SESSION['argent'] . "$</a></li>";
+            echo "<li><a href=ajouterauportemonaie.php><button class='btn btn-warning text-center'>Aprovisionner</button></a></li>";
+            echo "<li><a href=deconnexion.php><button class='btn btn-danger text-center'>Se déconnecter</button></a></li>";
+        } else {
+            echo "<li><a href=authentification.html><button class='btn btn-success '>Se connecter</button></a></li>";
+            echo "<li><a href=inscription.html><button class='btn btn-info text-center'>Inscription</button></a></li>";
+        } ?>
+        <li>
+            <form class="form-inline" role="form" action="resultat.php" method="POST">
+                <input type="search" class="form-control" name="achercher" placeholder="recherche">
+                <input type="submit" name="Cherche" class="btn btn-success  text-center">
+            </form>
+        </li>
+        </ul>
     </div>
-        </div>
+</div>
 </body>
 
 
--- verificationportem.php
+++ verificationportem.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Site d'enchères - Approvisionner </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/Projet.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-2.0.3.min.js"></script>
+</head>
+<body style="text-align: center">
+<iframe src="iframepanel.php?titre=Approvisioner" height="100" width="100%" name="panel" frameborder="0" ></iframe>
 <?php
 session_start();
 require_once('./stripe/config.php');
@@ -18,3 +29,6 @@ include("./include/mysqlapi.inc.php");
 echo '<h1>Vous avez ajouté '.$_POST['montant'].'$ à votre Porte Monaie </h1>';
 $_SESSION['argent'] = aprovisionner($_SESSION['pseudo'],$_POST['montant']);
 ?>
+<a href='index.php'><button class="btn btn-warning">Retour</button></a>
+</body>
+</html>
