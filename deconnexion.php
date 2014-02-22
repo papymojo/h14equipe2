408f1a5ea667e8ffae0f99a8b42cf1a53a5e8510
Emilie <emilie317@hotmail.com>
2014-02-21 20:12:44 -0500

    Carousel plus le Include Dependances.html

--- deconnexion.php
+++ deconnexion.php
@@ -5,18 +5,13 @@ session_destroy();
 ?>
 <html>
 <head>
-    <title></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/Projet.css">
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery-2.0.3.min.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <?php include('./include/Dependances.html'); ?>
 </head>
 <body>
 <iframe src="iframepanel.php?titre=Déconnection" height="100" width="100%" name="panel" frameborder="0" ></iframe>
     <div class="hero-unit">
         <h1>Vous êtes désormais déconnecté</h1>
-        <a href="./index.php"><button class="btn btn-warning text-center">Revenir à l'accueil</button></a>
+        <a href="./index.php"><button class="btn btn-warning text-center">Revenir à l'acceuil</button></a>
     </div>
 </body>
 </html>
