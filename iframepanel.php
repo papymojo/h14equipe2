99ab203e00b7bc7a3ca43df170553cb28a2a91f8
Emilie <emilie317@hotmail.com>
2014-03-07 10:40:35 -0500

    iframe

--- iframepanel.php
+++ iframepanel.php
@@ -18,11 +18,11 @@
         echo '<li><a href="index.php"><button class="btn btn-info text-center"><i class="icon-large icon-home icon-white"></i> Accueil
 </button></a></li>';
         if (isset($_SESSION['userid'])) {
-            echo "<li><a href='modificationutilisateur.php'><button class='btn btn-info text-center'><i class='icon-large icon-white icon-user'></i> " . $Session . "</button></a></li>";
-            echo "<li><a href=ajouterauportemonaie.php><button class='btn btn-warning text-center'><i class='icon-large icon-white  icon-shopping-cart'></i> " . number_format($_SESSION['argent'], 2, ",", " ") . "$</button></a></li>";
-            echo "<li><a href=deconnexion.php><button class='btn btn-danger text-center'><i class='icon-large icon-off icon-white'></i> Se déconnecter</button></a></li>";
+            echo "<li><a href='#'><button class='btn btn-info text-center' ata-toggle='tooltip' data-placement='bottom' title='Modifiez vos information personelles'><i class='icon-large icon-white icon-user'></i> " . $Session . "</button></a></li>";
+            echo "<li><a href=ajouterauportemonaie.php><button class='btn btn-warning text-center' data-toggle='tooltip' data-placement='bottom' title='Approvisionnez votre porte monaie'><i class='icon-large icon-white  icon-shopping-cart'></i> " . number_format($_SESSION['argent'], 2, ",", " ") . "$</button></a></li>";
+            echo "<li><a href=deconnexion.php><button class='btn btn-danger text-center' data-toggle='tooltip' data-placement='bottom' title='Déconnectez vous'><i class='icon-large icon-off icon-white'></i> Se déconnecter</button></a></li>";
         } else {
-            echo "<li><a href=authentification.php><button class='btn btn-success '>Se connecter</button></a></li>";
+            echo "<li><a href=authentification.php><button class='btn btn-success ' data-toggle='tooltip' data-placement='bottom' title='Se connecter pour effectuer des achats'>Se connecter</button></a></li>";
             echo "<li><a href=inscription.php><button class='btn btn-info text-center'>Inscription</button></a></li>";
         } ?>
         <li id="recherche">
