8ef381de2b68c68b46f5ac97836b1a2a210f7de0
benjaminbercy <benjamin.bercy@orange.fr>
2014-03-11 10:47:30 -0400

    cron utf-8

--- cron.php
+++ cron.php
@@ -1,7 +1,7 @@
 <?php
 include("./include/mysqlapi.inc.php");
 $headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 
 
 echo "start cron task";
