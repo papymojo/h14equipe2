ec059086ea20763cef82e46dbfdaeb3e1b422491
benjaminbercy <benjamin.bercy@orange.fr>
2014-03-14 10:58:27 -0400

    correction bug de ' dans nom du produit

--- verificationproduit.php
+++ verificationproduit.php
@@ -15,7 +15,7 @@
 session_start();
 include "./include/mysqlapi.inc.php";
 $dossier = './images/produits/' . rand(0, 2147483647) . '/';
-$id = insproduit($_POST['nom'], $_POST['prix'], $_POST['duree'], mysql_real_escape_string($_POST['description']), mysql_real_escape_string($dossier . $_POST['nom'] . '.jpg'), mysql_real_escape_string($_POST['etat']));
+$id = insproduit(mysql_real_escape_string($_POST['nom']), $_POST['prix'], $_POST['duree'], mysql_real_escape_string($_POST['description']), mysql_real_escape_string($dossier . $_POST['nom'] . '.jpg'), mysql_real_escape_string($_POST['etat']));
 if (isset($_FILES['image'])) {
     $extensions = array('.jpeg', '.jpg');
     if (in_array(strrchr($_FILES['image']['name'], '.'), $extensions)) {
